de0debfdde4d5efb7baf8694bb89238313501baf
lagilogi <wouter_s@hotmail.nl>
2026-06-30 11:34:51 +0200

    Added project files

---
+++ Phase-1-learn-c#-properly/3-file-organizer/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+namespace FileOrganizerApp
+{
+    static class Program
+    {
+        static void Main()
+        {
+            string rootPath = @"C:\tmp\";
+
+            string[] dirs = Directory.GetDirectories(rootPath, "*", SearchOption.AllDirectories);
+            string[] files = Directory.GetFiles(rootPath, "*.jpg", SearchOption.TopDirectoryOnly);
+
+            foreach (string dir in dirs)
+            {
+                Console.WriteLine(dir);
+            }
+
+            foreach (string file in files)
+            {
+                Console.WriteLine(Path.GetFileName(file));
+            }
+            Console.WriteLine($"File count: {files.Count()}");
+        }
+
+    }
+}
